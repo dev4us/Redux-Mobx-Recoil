351ee115392f51f72e48ff4335bbc2d8497c3365
dev4us <bronxofsoul@gmail.com>
2021-04-18 19:50:53 +0900

    refactor using typesafe-actions

--- redux/src/modules/counter.ts
+++ redux/src/modules/counter.ts
@@ -1,20 +1,18 @@
-// type
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+import { ActionType, createAction, createReducer } from 'typesafe-actions';
 
-// action
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  payload: diff
-});
+// 액션 type 선언
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+const INCREASE_BY = 'counter/INCREASE_BY';
 
-type CounterAction = 
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>;
+// 액션 생성 함수 선언
+export const increase = createAction(INCREASE)();
+export const decrease = createAction(DECREASE)();
+export const increaseBy = createAction(INCREASE_BY)<number>();
+
+// 액션 객체 타입 만들기
+const actions = {increase, decrease, increaseBy};
+type CounterAction = ActionType<typeof actions>;
 
 // state
 interface CounterState {
@@ -25,17 +23,11 @@ const initialState: CounterState = {
   count: 0
 }
 
-// reducer
-export default function counter(state: CounterState = initialState, action: CounterAction){
-  switch(action.type){
-    case INCREASE:
-      return { count: state.count + 1};
-    case DECREASE:
-      return { count: state.count - 1};
-    case INCREASE_BY:
-      return { count: state.count + action.payload};
-    default:
-      return state;
-  }
-}
+// 리듀서 만들기
+const counter = createReducer<CounterState, CounterAction>(initialState, {
+  [INCREASE]: state => ({count : state.count + 1}),
+  [DECREASE]: state => ({count: state.count - 1}),
+  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload})
+});
 
+export default counter;
